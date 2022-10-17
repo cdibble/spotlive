69fb871ec5c4b7e85ed8f46018edbf1455c9a0b5
Connor <cddibble@gmail.com>
2022-10-16 23:26:32 -0700

    fixes lookup_artist and get_playlists

--- spotlive/spotify.py
+++ spotlive/spotify.py
@@ -17,20 +17,22 @@ class Spot:
                 client_id=spotify_app_creds.get('client_id'),
                 client_secret=spotify_app_creds.get('client_secret'),
                 redirect_uri=spotify_app_creds.get('redirect_uri'),
-                scope="user-library-read")
+                scope="user-library-read,playlist-modify-private,playlist-modify-public")
             )
-        self.user_id = user_id
+        self.user_id = user_id if user_id else spotify_app_creds.get('user_id')
     def get_playlists(self, user: str = 'spotify'):
-        playlists = self.spot.user_playlists('spotify')
+        playlists = self.spot.user_playlists(user)
         # playlists = self.spot.current_user_playlists()
+        all_playlists=[]
         while playlists:
             for i, playlist in enumerate(playlists['items']):
-                print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+                # print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+                all_playlists.append(playlist)
             if playlists['next']:
                 playlists = self.spot.next(playlists)
             else:
                 playlists = None
-        return playlists
+        return all_playlists
     def set_user_playlists(self, user_id: str = None):
         if not user_id:
             user_id = self.user_id
@@ -45,26 +47,28 @@ class Spot:
             collaborative= collaborative,
             description = description
         )
-    def add_to_playlist(self, playlist_id: str = None, items: list = []):
+    def add_to_playlist(self, playlist_id: str = None, artists: list = []):
+        for artist in artists:
+            arts = self.lookup_artist(name = artist, return_type = 'track')
+            tracks = [x['uri'] for x in arts['tracks']['items']]
         self.spot.playlist_add_items(
             playlist_id=playlist_id,
-            items = items
+            items = tracks
         )
     def get_saved_tracks(self):
         results = self.spot.current_user_saved_tracks()
         for idx, item in enumerate(results['items']):
             track = item['track']
             print(idx, track['artists'][0]['name'], " – ", track['name'])
-    def lookup_artist(self, name: str):
-        name = 'Radiohead'
-        results = self.spot.search(q='artist:' + name, type='artist')
-        items = results['artists']['items']
-        if len(items) > 0:
-            artist = items[0]
-            print(artist['name'], artist['images'][0]['url'])
+    def lookup_artist(self, name: str, return_type: str = 'track'):
+        '''
+        return_type - the types of items to return. One or more of 'artist', 'album', 'track', 'playlist', 'show', and 'episode'. If multiple types are desired, pass in a comma separated string; e.g., 'track,album,episode'.
+        '''
+        results = self.spot.search(q='artist:' + name, type=return_type)
+        return results
 
 
 def main():
     with open('secrets/spotify_app_creds.json') as f:
         spotify_app_creds = json.loads(f.read())
-    spot = Spot(spotify_app_creds)
+    spot = Spot(spotify_app_creds, user_id = spotify_app_creds['user_id'])
