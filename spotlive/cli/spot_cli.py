1c57d4d9f637b72b92f2bce338a7c614fe6169e9
Connor <cddibble@gmail.com>
2022-11-05 10:54:02 -0700

    extend cli update command to use start_date and days_forward args

--- spotlive/cli/spot_cli.py
+++ spotlive/cli/spot_cli.py
@@ -3,7 +3,7 @@ from spotlive import SpotLive
 import json
 
 class SpotCli(object):
-    def __init__(self, tm_path, spotify_path):
+    def __init__(self, tm_path, spotify_path, start_date: str = None, days_forward: int = 30):
         try:
             self.ticketmaster_app_creds = self.get_creds(tm_path)
         except TypeError:
@@ -13,9 +13,13 @@ class SpotCli(object):
         except TypeError:
             self.spotify_app_creds = {}
         try:
+            print(f"here start_date: {start_date}")
+            print(f"days_forward: {days_forward}")
             self.spotlive = SpotLive(
                 spotify_app_creds = self.spotify_app_creds,
-                ticketmaster_app_creds = self.ticketmaster_app_creds
+                ticketmaster_app_creds = self.ticketmaster_app_creds,
+                start_date = start_date,
+                days_forward = days_forward
                 )
         except:
             self.spotlive = None
